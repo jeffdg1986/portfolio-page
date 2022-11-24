e03de7ad1b43d317e2f2628a78645e98d0aba631
JPaul <jeffdg1986@gmail.com>
2022-11-24 16:07:16 -0600

    commit to portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component } from 'react';
+class PortfolioPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() { 
+    return ( <>
+    <nav className='navBar'>
+      <div className='linkContainer'>
+      <a className='homeLink' href='https://www.google.com'>Home</a>
+      <a className='bioLink' href='https://www.google.com'>About Me</a>
+      <a className='projectLink4' href='https://www.google.com'>Survey Form</a>
+      <a className='projectLink3' href='https://www.google.com'>Percussion Machine</a>
+      <a className='projectLink2' href='https://jeffdg1986.github.io/timer-app/index.html'>Pomodoro Clock</a>
+      <a className='projectLink1' href='https://jeffdg1986.github.io/detailed_calculator/'>Calculator App</a>
+      </div>
+    </nav>
+    <section className='bioSection'>
+    <div className='welcome'>
+    <h2>Hi, I'm Jeff!</h2>
+    <p>I am a self taught software engineer with a focus on front end development. 
+      Check out my projects at the top right or more about my background below.</p>
     </div>
-  );
+    <div className='technologies'>Technologies Available</div>
+    </section>
+      <div className='about'>an area where I tell me chemistry to software story and provide a pdf copy of my resume</div>
+      <div className='projects'>a place where I have screenshot images of my projects with links to github io </div>
+    </> );
+  }
 }
+export default PortfolioPage ;
 
-export default App;
